fd81026e993344e103d756d6165db2dc0dc5d657
NguyenHoangVu-2004 <vunhph45875@fpt.edu.vn>
2024-01-21 23:22:41 +0700

    Code-ASM-DUANMAU-PH45875
    Code- Dự án mẫu - ASM

---
+++ admin/danhmuc/add.php
@@ -0,0 +1,24 @@
+<main>
+                <div class="title">
+                    <h1>THÊM MỚI LOẠI MẶT HÀNG</h1>
+                </div>
+                <div class="content">
+                    <form action="index.php?act=adddm" method="post">
+                        <div class="ip">
+                            <label for="">Mã loại :</label><br>
+                            <input type="text" name="maloai" disabled>
+                        </div>
+                        <div class="ip">
+                            <label for="">Tên loại :</label><br>
+                            <input type="text" name="tenloai" >
+                        </div>
+                        <div class="bt">
+                            <input type="submit" name="themmoi" value="THÊM MỚI">
+                            <input type="reset" value="NHẬP LẠI">
+                            <a href="danhsach_loaihang.html"><input type="button" value="DANH SÁCH"></a>
+                        </div>
+                    </form>
+                </div>
+            </main>
+        </header>
+    </div>
---
+++ admin/header.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Admin</title>
+    <link rel="stylesheet" href="../css/style.css">
+</head>
+
+<body>
+    <div class="boxcenter">
+        <header>
+            <div class="admin">
+                <h1>Admin</h1>
+            </div>
+            <div class="menu">
+                <ul>
+                    <li><a href="index.php">Trang chủ</a></li>
+                    <li><a href="index.php?act=adddm">Danh mục</a></li>
+                    <li><a href="index.php?act=addsp">Hàng hoá</a></li>
+                    <li><a href="index.php?act=dskh">Khách hàng</a></li>
+                    <li><a href="index.php?act=dsbl">Bình luận</a></li>
+                    <li><a href="index.php?act=thongke">Thống kê</a></li>
+                </ul>
+            </div>
